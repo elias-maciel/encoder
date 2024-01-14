e16af14c5319b16ce5f213f36dc434b1e4f8a80a
Elias Maciel <eliasfsilvamaciel@gmail.com>
2024-01-14 01:27:16 -0300

    fix: :bug: update go.mod and go.sum

--- domain/video_test.go
+++ domain/video_test.go
@@ -1,11 +1,12 @@
 package domain_test
 
 import (
-		"testing"
-		"github.com/stretchr/testify/require"
-		"encoder/domain"
-		"time"
-	)
+	"encoder/domain"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
 
 func TestValidateIfVideoIsEmpty(t *testing.T) {
 	video := domain.NewVideo()
@@ -25,4 +26,17 @@ func TestVideoIsNotAUuid(t *testing.T) {
 	err := video.Validate()
 
 	require.Error(t, err)
+}
+
+func TestVideoValidation(t *testing.T) {
+	video := domain.NewVideo()
+
+	video.ID = uuid.NewV4().String()
+	video.ResourceID = "a"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	err := video.Validate()
+
+	require.Nil(t, err)
 }
--- go.mod
+++ go.mod
@@ -1,8 +1,15 @@
 module encoder
 
-go 1.14
+go 1.21.5
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
-	github.com/stretchr/testify v1.6.1
+	github.com/satori/go.uuid v1.2.0
+	github.com/stretchr/testify v1.8.4
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
