17edf2dcb705a943a6a0c144ab681a460275d914
elias-maciel <eliasfsilvamaciel@gmail.com>
2024-01-14 01:08:22 -0300

    test: :white_check_mark: add tests

--- domain/video_test.go
+++ domain/video_test.go
@@ -4,10 +4,24 @@ import (
 		"testing"
 		"github.com/stretchr/testify/require"
 		"encoder/domain"
+		"time"
 	)
 
 func TestValidateIfVideoIsEmpty(t *testing.T) {
 	video := domain.NewVideo()
+	err := video.Validate()
+
+	require.Error(t, err)
+}
+
+func TestVideoIsNotAUuid(t *testing.T) {
+	video := domain.NewVideo()
+
+	video.ID = "abc"
+	video.ResourceID = "a"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
 	err := video.Validate()
 
 	require.Error(t, err)
