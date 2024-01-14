50f5c51167f77dd909601c693fd267bc117e9aec
Elias Maciel <eliasfsilvamaciel@gmail.com>
2024-01-14 15:46:18 -0300

    feat: :sparkles: add Fragment method to video_service

--- application/services/video_service.go
+++ application/services/video_service.go
@@ -7,12 +7,13 @@ import (
 	"io"
 	"log"
 	"os"
+	"os/exec"
 
 	"cloud.google.com/go/storage"
 )
 
 type VideoService struct {
-	Video *domain.Video
+	Video           *domain.Video
 	VideoRepository *repositories.VideoRepository
 }
 
@@ -60,4 +61,30 @@ func (v *VideoService) Download(bucketName string) error {
 	log.Printf("Video %v has been stored", v.Video.ID)
 
 	return nil
+}
+
+func (v *VideoService) Fragment() error {
+	err := os.Mkdir(os.Getenv("localStoragePath")+"/"+v.Video.ID, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	source := os.Getenv("localStoragePath" + "/" + v.Video.ID + ".mp4")
+	target := os.Getenv("localStoragePath" + "/" + v.Video.ID + ".frag")
+
+	cmd := exec.Command("mp4fragment", source, target)
+	output, err := cmd.CombinedOutput()
+
+	if err != nil {
+		return err
+	}
+	printOutput(output)
+
+	return nil
+}
+
+func printOutput(out []byte) {
+	if len(out) > 0 {
+		log.Printf("=====> Output: %s\n", string(out))
+	}
 }
