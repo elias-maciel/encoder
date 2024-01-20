f49d4858687fc9f4fb6e9ba08c3e236334ed3de6
Elias Maciel <eliasfsilvamaciel@gmail.com>
2024-01-20 13:54:16 -0300

    feat: :sparkles: Add video insertion functionality and start job

--- application/services/job_worker.go
+++ application/services/job_worker.go
@@ -3,7 +3,11 @@ package services
 import (
 	"encoder/domain"
 	"encoder/framework/utils"
+	"encoding/json"
+	"os"
+	"time"
 
+	uuid "github.com/satori/go.uuid"
 	"github.com/streadway/amqp"
 )
 
@@ -13,14 +17,59 @@ type JobWorkerResult struct {
 	Error   error
 }
 
-func JobWorker(messageChannel chan amqp.Delivery, returnChannel chan JobWorkerResult, jobService JobService, workerID int) {
+func JobWorker(messageChannel chan amqp.Delivery, returnChan chan JobWorkerResult, jobService JobService, job domain.Job, workerID int) {
 	for message := range messageChannel {
 		err := utils.IsJson(string(message.Body))
 
 		if err != nil {
-			returnChannel <- returnJobResult(domain.Job{}, message, err)
+			returnChan <- returnJobResult(domain.Job{}, message, err)
 			continue
 		}
+
+		err = json.Unmarshal(message.Body, &jobService.VideoService.Video)
+		jobService.VideoService.Video.ID = uuid.NewV4().String()
+
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		err = jobService.VideoService.Video.Validate()
+
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		err = jobService.VideoService.InsertVideo()
+
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		job.Video = jobService.VideoService.Video
+		job.OutputBucketPath = os.Getenv("outputBucketName")
+		job.ID = uuid.NewV4().String()
+		job.Status = "STARTING"
+		job.CreatedAt = time.Now()
+
+
+		_, err = jobService.JobRepository.Insert(&job)
+
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		jobService.Job = &job
+		err = jobService.Start()
+
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+		returnChan <- returnJobResult(job, message, err)
 	}
 }
 
